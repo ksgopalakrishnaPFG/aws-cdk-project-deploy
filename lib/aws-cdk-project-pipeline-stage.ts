6a07ef6eac56f23742c2b6cc784ae8ff0724389b
k006150 <ks.gopalakrishna@principal.com>
2021-10-08 00:44:22 +0530

    build fail resolving

--- lib/aws-cdk-project-pipeline-stage.ts
+++ lib/aws-cdk-project-pipeline-stage.ts
@@ -1,13 +1,13 @@
 import * as cdk from '@aws-cdk/core';
 import { CfnOutput } from '@aws-cdk/core';
-import { AwsCdkProjectStack } from './aws-cdk-project-stack';
+import { AwsCdkProjectDeployStack } from './aws-cdk-project-deploy-stack';
 
 export class AwsCdkProjectPipelineStage extends cdk.Stage {
 public readonly urlOutput : CfnOutput;
   constructor(scope: cdk.Construct, id: string, props?: cdk.StageProps) {
     super(scope, id, props);
 
-    const stage  = new AwsCdkProjectStack(this,'FirstWebService');
+    const stage  = new AwsCdkProjectDeployStack(this,'FirstWebService');
 
     new CfnOutput(this,'StageUrlSuffix',{
       value : stage.urlSuffix
