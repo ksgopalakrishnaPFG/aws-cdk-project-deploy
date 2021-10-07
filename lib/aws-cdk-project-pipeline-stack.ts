712fc39aa36e1a87b59f79e80ae3f07c69ee0c18
k006150 <ks.gopalakrishna@principal.com>
2021-10-08 00:07:04 +0530

    updated the branch detail

--- lib/aws-cdk-project-pipeline-stack.ts
+++ lib/aws-cdk-project-pipeline-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as codepipeline from "@aws-cdk/aws-codepipeline";
 import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
-import * as pipe_line from '@aws-cdk/pipelines'
+import * as pipe_line from "@aws-cdk/pipelines";
 import { SecretValue } from "@aws-cdk/core";
 import { pipeline } from "stream";
 
@@ -12,30 +12,30 @@ export class AwsCdkProjectPipelineStack extends cdk.Stack {
     const sourceArtifact = new codepipeline.Artifact();
     const cloudAssemblyArtifact = new codepipeline.Artifact();
 
-    const pipeline = new pipe_line.CdkPipeline(this,'PipeLine',{
-        //the pipeline name
-        pipelineName : 'MyPipeLine',
+    const pipeline = new pipe_line.CdkPipeline(this, "PipeLine", {
+      //the pipeline name
+      pipelineName: "MyPipeLine",
+      cloudAssemblyArtifact,
+
+      //this the source information
+      sourceAction: new codepipeline_actions.GitHubSourceAction({
+        actionName: "MyGitHubSource",
+        oauthToken: SecretValue.secretsManager("AWS_CDK_Piepline_token"),
+        output: sourceArtifact,
+        owner: "ksgopalakrishnaPFG",
+        repo: "aws-cdk-project-deploy",
+        branch: "master",
+      }),
+
+      //how to build
+      synthAction: pipe_line.SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
         cloudAssemblyArtifact,
 
-        //this the source information
-        sourceAction : new codepipeline_actions.GitHubSourceAction({
-            actionName:'MyGitHubSource',
-            oauthToken :SecretValue.secretsManager('AWS_CDK_Piepline_token'),
-            output: sourceArtifact,
-            owner:'ksgopalakrishnaPFG',
-            repo:'aws-cdk-project-deploy'
-
-        }),
-
-        //how to build
-        synthAction : pipe_line.SimpleSynthAction.standardNpmSynth({
-            sourceArtifact,
-            cloudAssemblyArtifact,
-
-            //we need to build the typescript contents
-            buildCommand: 'npm run build'
-        })
-    })
+        //we need to build the typescript contents
+        buildCommand: "npm run build",
+      }),
+    });
     // The code that defines your stack goes here
   }
 }
